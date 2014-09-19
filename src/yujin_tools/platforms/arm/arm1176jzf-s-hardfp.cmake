b40595a8d7a14a91e4e48a2de52ec9b461c29b68
Daniel Stonier <d.stonier@gmail.com>
2014-09-19 16:53:00 +0900

    0.2.57

---
+++ src/yujin_tools/platforms/arm/arm1176jzf-s-hardfp.cmake
@@ -0,0 +1,18 @@
+###############################################################################
+# Family : arm
+# Platform : arm1176jzf-s with hardfp
+# Notes: use with armel compilers.
+###############################################################################
+
+# Some useful custom variables that uniquely define this platform module
+set(PLATFORM_FAMILY "arm" CACHE STRING "Platform family, usually referring to intel/arm etc.")
+set(PLATFORM_NAME "arm1176jzf-s" CACHE STRING "Platform name, usually referring to the cpu architecture.")
+
+# Flags
+set(PLATFORM_CXX_FLAGS "-march=armv6 -mtune=arm1176jzf-s -pipe -mfloat-abi=hard -mfpu=vfp" CACHE STRING "Compile flags specific to this platform.")
+set(PLATFORM_LINK_FLAGS "" CACHE STRING "Link flags specific to this platform.")
+
+# Custom software settings
+set(ECL_EIGEN_IS_INTERNAL_3_2_1 TRUE CACHE BOOL "Use ecls internal eigen.")
+# Nanomsg - disable it's support for gcc builtins
+set(NN_DISABLE_ATOMIC_GCC_BUILTINS TRUE CACHE BOOL "Disable atomic gcc builtins.")
