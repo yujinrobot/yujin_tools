9ce6758ce4802abc709ad55c9cbd1262169353b2
Daniel Stonier <d.stonier@gmail.com>
2016-08-22 10:16:20 +0900

    yujin_ansible migrated to yujin_ansible_playbooks

--- src/yujin_tools/__init__.py
+++ src/yujin_tools/__init__.py
@@ -1,8 +1,4 @@
 
-from . import ansible
-from . import ansible_common
-from . import ansible_pc
-
 from .init_workspace import init_workspace
 from .init_build import init_build
 from .make import make_main
