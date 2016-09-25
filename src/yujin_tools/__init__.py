a1f768784f9d52c5eee9b09e257dce74e6a9ec6d
Daniel Stonier <d.stonier@gmail.com>
2016-09-25 16:02:48 +0900

    0.4.38

--- src/yujin_tools/__init__.py
+++ src/yujin_tools/__init__.py
@@ -5,4 +5,4 @@ from .make import make_main
 from .make_isolated import make_isolated_main
 
 # should be before imports, but current pep8 barfs at this
-__version__ = '0.4.37'
+__version__ = '0.4.38'
