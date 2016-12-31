aff5aad88c4fa6c142eb060dd55fb1e83606a6dd
Daniel Stonier <d.stonier@gmail.com>
2016-12-31 14:49:54 +0900

    [infra] drop yujin_tools module dependency from setup.py

--- src/yujin_tools/__init__.py
+++ src/yujin_tools/__init__.py
@@ -3,4 +3,3 @@ from .init_workspace import init_workspace
 from .init_build import init_build
 from .make import make_main
 from .make_isolated import make_isolated_main
-from .version import __version__
